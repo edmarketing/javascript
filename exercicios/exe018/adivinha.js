56e01d69d12a4c6a13043bd27b06bc064aa9ccfa
EdMarketing <edjmaria2021@gmail.com>
2023-02-26 10:56:46 -0300

    Atualizando exe018

--- exercicios/exe018/adivinha.js
+++ exercicios/exe018/adivinha.js
@@ -2,6 +2,7 @@
 let res = document.querySelector('section#result')
 let computador = 0
 let jogador = 0
+
 function sortear() {
     let min = 1 // As variáveis declaradas aqui dentro são LOCAIS, e só funcionam dentro da função onde foram declaradas
     let max = 100
@@ -9,6 +10,7 @@ function sortear() {
     let aleatorio = Math.random()
     computador = min + Math.trunc(dif * aleatorio)
 }
+
 function jogar() {
     jogador = Number(window.prompt('Qual é o seu palpite?'))
     if (jogador < computador) {
@@ -19,3 +21,4 @@ function jogar() {
         res.innerHTML += `<p><strong>PARABÉNS!</strong> Você acertou! Eu tinha sorteado o valor ${computador}!</p>`
         document.getElementById('botao').style.visibility = 'hidden'
     }
+}
