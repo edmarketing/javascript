63cd2440afa2da45f77a86f5075faea0ff72d5a6
EdMarketing <edjmaria2021@gmail.com>
2023-02-28 17:25:51 -0300

    Alterando exe002

--- exercicios/exe009/scripts.js
+++ exercicios/exe009/scripts.js
@@ -3,8 +3,8 @@ let res = document.querySelector('section#result')
 // As variáveis declaradas aqui fora são consideradas GLOBAIS e funcionam dentro do programa inteiro
 
 function contar() {
-    contador ++ // É a mesma coisa que contador = contador + 1
-    res.innerHTML = `<p>O contador está com <mark>${contador}</mark> cliques.</p>`
+  contador ++ // É a mesma coisa que contador = contador + 1 
+  res.innerHTML = '<p>O contador está com <mark>${contador}</mark> cliques.</p>'
 }
 
 function zerar() {
