52623c5290679df8006c321f842d94d5cba2e04b
EdMarketing <edjmaria2021@gmail.com>
2023-03-03 00:13:27 -0300

    Criar desafio01

---
+++ desafios/desafio01/acoes.js
@@ -0,0 +1,8 @@
+function acao1() {
+    window.alert('Você clicou no 1º botão')
+}
+function acao2() {
+    window.alert('Você clicou no 2º botão')
+}
+function acao3() {
+    window.alert('Você clicou no 3º botão')
