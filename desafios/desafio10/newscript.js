28bfa79c9204589bb65078b149d219f9d8b11e52
EdMarketing <edjmaria2021@gmail.com>
2023-05-18 01:00:25 -0300

    Criando Desafio011

---
+++ desafios/desafio10/newscript.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let button = document.getElementById('calcular');
+//let button_limpar = document.getElementById('limpar');
+
+button.addEventListener('click',calculo);
+//button_limpar.addEventListener('click',limpar);
+
+function calculo(e){
+    e.preventDefault();//para impedir que a página HTML sofra refresh
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let num3 = parseFloat(document.getElementById('num3').value);
+    let delta = Math.pow(num2,2) - 4*num1*num3;
+    alert(`O delta é: ${delta}`);
+    if(delta > 0){
+        let raiz1 = (-num2 + Math.sqrt(delta) )/(2*num1);
+        let raiz2 = (-num2 - Math.sqrt(delta) )/(2*num1);
+        document.getElementById("resultado").innerHTML = "Raiz 1 = " + raiz1 + "<br>" + "Raiz 2 = " + raiz2; 
+        
+    }else if (delta===0){
+            let raizUnica = (-num2 + Math.sqrt(delta) )/(2*num1);
+            document.getElementById("resultado").innerHTML = "Raiz única = " + raizUnica;
+    }else if (delta < 0){
+        document.getElementById("resultado").innerHTML = "Delta < 0, não existem raízes no conjunto dos números reais.";
+    }
+}
